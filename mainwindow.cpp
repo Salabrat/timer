5c2bd2b61324a249575c758b61a0999ba09cd605
Salabrat <salabrat66@gmail.com>
2023-05-31 13:57:44 +0400

    ColorFont,BG,ImagetoBG.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,16 @@
 #include "mainwindow.h"
+#include <QMainWindow>
+#include <QLabel>
+#include <QTimer>
+#include <QFont>
+#include <QColor>
+#include <QAction>
+#include <QMenu>
+#include <QFileDialog>
+#include <QFontDialog>
+#include <QMenuBar>
+
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,8 +27,59 @@ MainWindow::MainWindow(QWidget *parent)
     });
 
     timer->start(1000);
+
+    // Создание меню
+    QMenuBar *menuBar = new QMenuBar(this);
+    setMenuBar(menuBar);
+
+    QMenu *formatMenu = menuBar->addMenu("Формат");
+
+    QAction *fontAction = new QAction("Изменить шрифт", this);
+    connect(fontAction, &QAction::triggered, this, &MainWindow::changeFont);
+    formatMenu->addAction(fontAction);
+
+    QAction *fontColorAction = new QAction("Изменить цвет шрифта", this);
+    connect(fontColorAction, &QAction::triggered, this, &MainWindow::changeFontColor);
+    formatMenu->addAction(fontColorAction);
+
+    QAction *bgColorAction = new QAction("Изменить цвет заднего фона", this);
+    connect(bgColorAction, &QAction::triggered, this, &MainWindow::changeBackgroundColor);
+    formatMenu->addAction(bgColorAction);
+
+    QAction *bgImageAction = new QAction("Установить фоновое изображение", this);
+    connect(bgImageAction, &QAction::triggered, this, &MainWindow::setBackgroundImage);
+    formatMenu->addAction(bgImageAction);
 }
 
 MainWindow::~MainWindow()
 {
+}
+
+void MainWindow::changeFont()
+{
+    bool ok;
+    QFont font = QFontDialog::getFont(&ok, label->font(), this);
+    if (ok)
+        label->setFont(font);
+}
+
+void MainWindow::changeFontColor()
+{
+    QColor color = QColorDialog::getColor(label->palette().color(QPalette::WindowText), this);
+    if (color.isValid())
+        label->setStyleSheet(QString("color: %1;").arg(color.name()));
+}
+
+void MainWindow::changeBackgroundColor()
+{
+    QColor color = QColorDialog::getColor(label->palette().color(QPalette::Window), this);
+    if (color.isValid())
+        setStyleSheet(QString("background-color: %1;").arg(color.name()));
+}
+
+void MainWindow::setBackgroundImage()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Выберите фоновое изображение", "", "Изображения (*.png *.jpg)");
+    if (!fileName.isEmpty())
+        setStyleSheet(QString("background-image: url(%1); background-repeat: no-repeat; background-position: center;").arg(fileName));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,12 @@
 #include <QMainWindow>
 #include <QLabel>
 #include <QTimer>
+#include <QFont>
+#include <QColor>
+#include <QColorDialog>
+#include <QAction>
+#include <QMenu>
+#include <QFileDialog>
 
 class MainWindow : public QMainWindow
 {
@@ -13,10 +19,21 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void changeFont();
+    void changeFontColor();
+    void changeBackgroundColor();
+    void setBackgroundImage();
+
 private:
     QLabel *label;
     QTimer *timer;
     int count;
+
+    QAction *fontAction;
+    QAction *fontColorAction;
+    QAction *backgroundColorAction;
+    QAction *backgroundImageAction;
 };
 
 #endif // MAINWINDOW_H
